2be1dd0f7022754193d044b9c1992115585c7765
paulwilljones <paul.jones@dyson.com>
2019-05-24 10:17:28 +0200

    Create AWS EKS cluster through CDK

--- lib/typescript-cdk-stack.ts
+++ lib/typescript-cdk-stack.ts
@@ -4,6 +4,15 @@ export class TypescriptCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.VpcNetwork(this, 'VPC');
+
+    const cluster = new eks.Cluster(this, 'EKSCluster', {
+      vpc
+    });
+
+    cluster.addCapacity('Nodes', {
+      instanceType: new ec2.InstanceType('t2.medium'),
+      desiredCapacity: 1,  // Raise this number to add more nodes
+    });
   }
 }
